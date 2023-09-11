f16f7ae3697b88aa494c484d0dc5ec39161bc672
Alisa Jin <alisajin@Alisas-MacBook-Pro-2.local>
2023-09-11 16:47:56 -0700

    Change name from OAuth to Login in component export

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css';
-import OAuth from './OAuth';
+import Login from './Login';
 
 function App() {
 
   return (
     <>
-      <OAuth />
+      <Login />
     </>
   )
 }
