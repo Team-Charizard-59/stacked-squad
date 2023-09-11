75ba8327fd97a486e576267435d980979fc74431
Alisa Jin <alisajin@Alisas-MacBook-Pro-2.local>
2023-09-11 15:44:10 -0700

    Add tailwind css config file and dependancy

---
+++ src/OAuth.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function OAuth() {
+  return (
+    <div className='flex flex-col'>
+        <h1>Login/SignUp</h1>
+        <input placeholder='Username/Email'></input>
+        <input placeholder='Password'></input>
+    </div>
+  )
+}
+
+export default OAuth
