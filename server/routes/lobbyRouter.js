3b3a54c17f034efcb7f860c6ebf17f6879785b8b
Irene Wang <ireneziwang@gmail.com>
2023-09-12 13:28:05 -0700

    set up router file. needs more middle added to complete functionality

--- server/routes/lobbyRouter.js
+++ server/routes/lobbyRouter.js
@@ -3,12 +3,54 @@ import lobbyController from '../controllers/lobbyController'
 
 const router = express.Router();
 
-router.post('/signup',
-  userController.signup,
-  (req,res) => res.status(201));
+// GET /lobby/
+// Get all lobbies from database
+router.get('/lobby/',
+  lobbyController.getAllLobbies,
+  (req,res) => res.status(200));
 
-router.post('/login',
-  userController.login,
+// GET /lobby/:lobbyId
+// Get a lobby from database by lobby ID
+router.get('/lobby/:lobbyId',
+  lobbyController.getLobbyByLobbyID,
   (req, res) => res.status(200));
 
-export default router;```
+// GET /lobby/:userId
+// Get all lobbies of user
+router.get('/lobby/:userId',
+  lobbyController.getLobbiesOfUser,
+  (req, res) => res.status(200));
+
+// Get all lobbies created by user
+// GET /lobby/createdBy/:userId
+route.get('/lobby/createdBy/:userId',
+  (lobbyController.getLobbiesCreatedByUser)
+)
+
+// POST /lobby/create
+// Create a lobby
+route.get('/lobby/create',
+  (lobbyController.createLobby)
+)
+
+// PATCH /lobby/edit:lobbyId
+// Edit a lobby
+route.get('/lobby/edit:lobbyId',
+  (lobbyController.editLobby)
+)
+
+
+// PATCH /lobby/join:userId
+// Add a user to a lobby
+route.get('/lobby/join:userId',
+  (lobbyController.joinLobby)
+)``
+
+
+// DELETE /lobby/delete:lobbyId
+// Delete a lobby
+route.get('/lobby/delete:lobbyId',
+  (lobbyController.deleteLobby)
+)
+
+export default router;
