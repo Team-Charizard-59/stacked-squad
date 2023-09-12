b796d1a5bc563198004db5ee16bce3dce09935a7
Hernan Damazo <raulclassico7@gmail.com>
2023-09-12 16:26:57 -0700

    Navbar creation

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css';
 import Login from './Login';
+import NavBar from './Navbar';
 
 function App() {
 
   return (
     <>
+      <NavBar/>
       <Login />
     </>
   )
