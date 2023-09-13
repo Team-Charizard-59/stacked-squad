a92ea4d5aeca5c1e2b7639e12437afc42f4acf25
Irene Wang <ireneziwang@gmail.com>
2023-09-13 12:31:29 -0700

    'your games' card styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,21 @@ import { Link } from 'react-router-dom';
 import UserGames from './Components/ui/userGames';
 import Login from './Login';
 import NavBar from './Navbar';
+import Hero from './Components/ui/Hero.jsx';
+
 
 function App() {
   return (
-    <div className='w-screen bg-base-100 h-screen'>
+    <div className='w-screen bg-base-100 h-full'>
+      <Link to={'/login'}>CLICK TO GO TO LOGIN</Link>
       <NavBar />
+          {/* <Hero /> */}
       <header className=''>
-        <h2>Stacked Squad</h2>
         <div id='main-container' className='flex  gap-4 mx-10'>
           <Feed />
           <UserGames />
         </div>
       </header>
-      <Link to={'/login'}>CLICK TO GO TO LOGIN</Link>
     </div>
   );
 }
--- src/Components/ui/userGames.jsx
+++ src/Components/ui/userGames.jsx
@@ -7,19 +7,35 @@ function UserGames (){
     const mode = game.mode;
     const uniqueKey = `game-${title}-${mode}`
     currentGames.push(
+      // <div
+      //   key={uniqueKey}
+      //   className="border-black border-2 my-2 p-2 rounded-xl">
+      //   <div className="">
+      //     <header className="flex justify-between items-center pr-4 pb-2 font-medium">
+      //       <p className="text-lg">{title}</p>
+      //       <p className="text-xs">{mode}</p>
+      //     </header>
+      //   </div>
+      //   <div className="flex gap-2">
+      //     <button className="btn btn-active">Del</button>{' '}
+      //     <button className="btn btn-active">Edit</button>{' '}
+      //     <button className="btn btn-active">Start</button>{' '}
+      //   </div>
+      // </div>
+
       <div
         key={uniqueKey}
-        className="border-black border-2 my-2 p-2 rounded-xl">
-        <div className="">
-          <header className="flex justify-between items-center pr-4 pb-2 font-medium">
-            <p className="text-lg">{title}</p>
-            <p className="text-xs">{mode}</p>
-          </header>
-        </div>
-        <div className="flex gap-2">
-          <button className="btn btn-active">Del</button>{' '}
-          <button className="btn btn-active">Edit</button>{' '}
-          <button className="btn btn-active">Start</button>{' '}
+        className='lobbyContainer card w-96 bg-neutral text-neutral-content m-5 '>
+        <div className='card-body items-center text-center'>
+          <p className="card-title">{title}</p>
+          <p className="text-xs">{mode}</p>
+            <div className='card-actions flex flex-end w-full'>
+              <button className="btn btn-ghost btn-xs justify-self-end">Close Lobby</button>{' '}
+              <div classNmae="flex ">
+                <button className='btn btn-active'>Edit</button>
+                <button className='btn btn-primary'>Start</button>
+              </div>
+            </div>
         </div>
       </div>
     );
